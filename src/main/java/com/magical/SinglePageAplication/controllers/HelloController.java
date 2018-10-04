a5fdeb8d8b1988429bacb37f3e31d8a4325daf4d
al-makarov <a.makarov.tmb@ya.ru>
2018-10-05 00:01:11 +0300

    security add

--- src/main/java/com/magical/SinglePageAplication/controllers/HelloController.java
+++ src/main/java/com/magical/SinglePageAplication/controllers/HelloController.java
@@ -1,12 +1,14 @@
 package com.magical.SinglePageAplication.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class HelloController {
     @RequestMapping("/")
+    @GetMapping(value = {"/", "/index"})
     String index() {
         return "index";
     }
---
+++ src/main/java/com/magical/SinglePageAplication/security/SpringSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.magical.SinglePageAplication.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AccessDeniedHandler accessDeniedHandler;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/**","/").hasAnyRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and()
+                .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+    }
+
+    // создаем пользоватлелей, admin и user
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("password").roles("ADMIN");
+    }
+}
